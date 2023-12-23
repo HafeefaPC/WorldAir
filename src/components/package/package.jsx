8ba4166519628b9c56226f8ccfcf26d21bb78151
Hafeefa <hafeefapc2003@gmail.com>
2023-12-23 22:06:03 +0530

    home page complete

--- src/components/package/package.jsx
+++ src/components/package/package.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 
-function Package({ name, img, para }) {
-    return (
-        <div className="flex justify-center items-center rounded-3xl bg-white h-60 mx-4 mb-4">
-            <div>
-          <img
-            src={img}
-            className="w-[233px] h-[211px] absolute mt-[3px] lg:mr-[27rem] sm:mr-[22rem] rounded-3xl"
-            alt={name}
-          />
-          </div>
-          <div className="flex ml-3 mt-5 lg:gap-0">
-            <p className="font-montserrat text-[24px] font-semibold leading-29 tracking-wider ml-[18rem] text-white mb-[1rem]">
-              {name}
-            </p>
-            <p className="font-sans text-base font-semibold leading-tight tracking-tight ml-[18rem] text-[#94a3b8]">
-              {para}
-            </p>
-          </div>
-        </div>
-      );
-    }
-    
+function Package({ img, name, para }) {
+  return (
+    <div className="max-w-xs max-h-100 bg-white border border-gray-200 rounded-lg shadow overflow-hidden">
+      <a href="#">
+        <img className="w-full h-40 object-cover" src={img} alt="" />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            {name}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 overflow-hidden">{para}</p>
+      </div>
+    </div>
+  );
+}
 
 export default Package;
--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
 
-export default function contact() {
-  return (
-    <div>
-      
+const Contact = () => (
+  <div className="background-container">
+    {/* Your content goes here */}
+    <div className="transparent-rectangle">
+      {/* Your content goes here */}
+      <img src="C:/Users/91812/OneDrive/Desktop/WorldAir/Images/contact.jpg" alt="Contact Background" />
+      <h1>We're here for you</h1>
+      <div className="contact-info">
+        <div className="contact-item">
+          <i className="fas fa-envelope"></i>
+          <span></span>
+          <span>example@example.com</span>
+        </div>
+        <div className="contact-item">
+          <i className="fas fa-phone"></i>
+          <span></span>
+          <span>+91 99466 8466</span>
+        </div>
+        <div className="contact-item">
+          <i className="fas fa-map-marker-alt"></i>
+          <span></span>
+          <span>
+            Arafa Complex<br />
+            Near Bus Stand<br />
+            Kunnamkulam
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  </div>
+);
+
+export default Contact;
