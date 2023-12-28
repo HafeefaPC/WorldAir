a648a7d594942f9343d35082ab57da3170320254
Hafeefa <hafeefapc2003@gmail.com>
2023-12-28 11:39:26 +0530

    small changes

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,37 +1,43 @@
-import React from 'react';
-import './Contact.css';
-import contact from "../../assets/Images/contact.jpg"
+import React from "react";
+import "./Contact.css";
+import contact from "../../assets/Images/contact.jpg";
+import Navbar from "../../components/Navbar/navbar";
+import Footer from "../../components/Footer/footer"
 
 const Contact = () => (
-  <div className="background-container">
-   
-    <div className="transparent-rectangle">
-  
-      <img src={contact} alt="Contact Background" />
-      <h1>We're here for you</h1>
-      <div className="contact-info">
-        <div className="contact-item">
-          <i className="fas fa-envelope"></i>
-          <span></span>
-          <span>example@example.com</span>
-        </div>
-        <div className="contact-item">
-          <i className="fas fa-phone"></i>
-          <span></span>
-          <span>+91 99466 8466</span>
-        </div>
-        <div className="contact-item">
-          <i className="fas fa-map-marker-alt"></i>
-          <span></span>
-          <span>
-            Arafa Complex<br />
-            Near Bus Stand<br />
-            Kunnamkulam
-          </span>
+  <>
+    <Navbar />
+    <div className="background-container">
+      <div className="transparent-rectangle">
+        <img src={contact} alt="Contact Background" />
+        <h1>We're here for you</h1>
+        <div className="contact-info">
+          <div className="contact-item">
+            <i className="fas fa-envelope"></i>
+            <span></span>
+            <span>example@example.com</span>
+          </div>
+          <div className="contact-item">
+            <i className="fas fa-phone"></i>
+            <span></span>
+            <span>+91 99466 8466</span>
+          </div>
+          <div className="contact-item">
+            <i className="fas fa-map-marker-alt"></i>
+            <span></span>
+            <span>
+              Arafa Complex
+              <br />
+              Near Bus Stand
+              <br />
+              Kunnamkulam
+            </span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+    <Footer/>
+  </>
 );
 
 export default Contact;
--- src/pages/Services/service.jsx
+++ src/pages/Services/service.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import Umra from "../../assets/Images/Umra.png"
-import Hotelbooking from "../../assets/Images/Hotelbooking.jpg"
-import visa from "../../assets/Images/visa.jpg"
-import './services.css';
+import React from "react";
+import Umra from "../../assets/Images/Umra.png";
+import Hotelbooking from "../../assets/Images/Hotelbooking.jpg";
+import visa from "../../assets/Images/visa.jpg";
+import Navbar from "../../components/Navbar/navbar";
+import Footer from "../../components/Footer/footer";
+import "./services.css";
 
 const Service = () => {
   return (
     <div>
+      <Navbar />
       <div className="rectangle-behind"></div>
       <div className="rectangle-box">
         <div className="white-box">
@@ -16,17 +19,50 @@ const Service = () => {
           <div className="content-box">
             <div className="card">
               <h2>Accommodation</h2>
-              <p><br />Hotel Reservation<br />Service Apartments<br />House Boat Stay<br />Home Stays<br /><br /></p>
+              <p>
+                <br />
+                Hotel Reservation
+                <br />
+                Service Apartments
+                <br />
+                House Boat Stay
+                <br />
+                Home Stays
+                <br />
+                <br />
+              </p>
             </div>
             {/* first service above */}
             <div className="card">
               <h2>Voyages</h2>
-              <p><br />Inbound & Outbound Tours<br />Incentive Tours<br />Village Tours<br />Botanica Tours<br />Cruises Holidays</p>
+              <p>
+                <br />
+                Inbound & Outbound Tours
+                <br />
+                Incentive Tours
+                <br />
+                Village Tours
+                <br />
+                Botanica Tours
+                <br />
+                Cruises Holidays
+              </p>
             </div>
             {/* second service above */}
             <div className="card">
               <h2>Other Services</h2>
-              <p><br />Car/Coach Rentals<br />Air Tickets<br />Conferences<br />Foreign Exchange<br /><br /></p>
+              <p>
+                <br />
+                Car/Coach Rentals
+                <br />
+                Air Tickets
+                <br />
+                Conferences
+                <br />
+                Foreign Exchange
+                <br />
+                <br />
+              </p>
             </div>
             {/* third service above */}
           </div>
@@ -34,7 +70,9 @@ const Service = () => {
       </div>
 
       <div className="class-for-packages">
-        <h1><b>Exclusive Packages</b></h1>
+        <h1>
+          <b>Exclusive Packages</b>
+        </h1>
       </div>
 
       <div className="outer-package">
@@ -42,8 +80,18 @@ const Service = () => {
           <div className="package-content">
             <img src={Umra} alt="Umra Package" />
             <div className="package-description">
-              <h2>Embark on a Sacred Pilgrimage: Discover Our Hajj and Umrah Packages</h2>
-              <p><br />As the call to pilgrimage resonates, We invites you to experience the spiritual journey of a lifetime with our meticulously crafted Hajj/Umrah package. We are dedicated to providing an unparalleled experience that combines faith, comfort, and convenience.</p>
+              <h2>
+                Embark on a Sacred Pilgrimage: Discover Our Hajj and Umrah
+                Packages
+              </h2>
+              <p>
+                <br />
+                As the call to pilgrimage resonates, We invites you to
+                experience the spiritual journey of a lifetime with our
+                meticulously crafted Hajj/Umrah package. We are dedicated to
+                providing an unparalleled experience that combines faith,
+                comfort, and convenience.
+              </p>
             </div>
           </div>
         </div>
@@ -51,8 +99,18 @@ const Service = () => {
         <div className="package pack2">
           <div className="package-content">
             <div className="package-description">
-              <h2>Immerse Yourself in Luxe Living: Unveil Our Enchanting Hotel Packages!</h2>
-              <p><br />Prepare for an extraordinary escape with our exclusive hotel booking packages. Elevate your journey from ordinary to extraordinary as we invite you to discover unparalleled comfort and style. Find comfort and convenience tailored to your preferences and budget.</p>
+              <h2>
+                Immerse Yourself in Luxe Living: Unveil Our Enchanting Hotel
+                Packages!
+              </h2>
+              <p>
+                <br />
+                Prepare for an extraordinary escape with our exclusive hotel
+                booking packages. Elevate your journey from ordinary to
+                extraordinary as we invite you to discover unparalleled comfort
+                and style. Find comfort and convenience tailored to your
+                preferences and budget.
+              </p>
             </div>
             <img src={Hotelbooking} alt="Hotel Booking" />
           </div>
@@ -62,13 +120,23 @@ const Service = () => {
           <div className="package-content">
             <img src={visa} alt="Visa Package" />
             <div className="package-description">
-              <h2>Unlock Your Global Adventure: Visa Packages for Effortless Travel!</h2>
-              <p><br />Embark on your journey to far-flung destinations with ease, courtesy of our comprehensive Visa Packages. Navigating the world is made simple, ensuring your travel dreams become reality without the hassle.</p>
+              <h2>
+                Unlock Your Global Adventure: Visa Packages for Effortless
+                Travel!
+              </h2>
+              <p>
+                <br />
+                Embark on your journey to far-flung destinations with ease,
+                courtesy of our comprehensive Visa Packages. Navigating the
+                world is made simple, ensuring your travel dreams become reality
+                without the hassle.
+              </p>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
-}
-export default Service
+};
+export default Service;
